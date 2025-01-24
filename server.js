c17d6dea9a0ff18758ce5dba4abe99efc7d6f6b8
Hanz <hsamonte@academic.rrc.ca>
2025-01-24 10:23:13 -0600

    Created a post message service, added bodyParser since Express can't parse the body.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // Assigning Express framework to a var
 var express = require('express')
+var bodyParser = require('body-parser')
 // Using Express on a var
 var app = express()
 
 
 // Serving a static file for Express (index.html)
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
 
 var messages = [
     {name: 'Jin', message: "BALLER"},
@@ -15,6 +19,12 @@ var messages = [
 // Handling GET Requests
 app.get('/messages', (req, res) => {
     res.send(messages)
+})
+
+// Handling POST Requests
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 
