0b2098791bc17461a353f40ac78db30226e02a5f
Hanz <hsamonte@academic.rrc.ca>
2025-01-24 00:19:43 -0600

    Created a 'getMessages' service endpoint, allowing us to get messages from back-end to front-end by handling GET requests.

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var app = express()
 
 // Serving a static file for Express (index.html)
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: 'Jin', message: "BALLER"},
+    {name: 'Ronnie', message: "hi"}
+]
+
+// Handling GET Requests
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+
 // Start the Express server and listen for port 3000
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
